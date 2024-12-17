bd4798eedc5e229b371e24e6cfe2d7656cc3a8b9
BaslealAkliluNeymar <baslealaklilu@gmail.com>
2024-12-17 16:57:25 +0300

    first

---
+++ app/font.ts
@@ -0,0 +1,4 @@
+import { Inter,Lusitana } from 'next/font/google';
+
+export const font = Inter({ subsets: ['latin'] });
+export const fontLusitana = Lusitana({ subsets: ['latin'], weight: ['400','700'] });
